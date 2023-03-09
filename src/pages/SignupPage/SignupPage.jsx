1d30a808e307e0103cffb197c371636da914e325
Frank Gimeno <frankgimeno3@gmail.com>
2023-03-09 14:16:56 +0100

    dsf

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -61,7 +61,7 @@ function SignupPage() {
       .catch((error) => {
         // If the request resolves with an error, set the error message in the state
         const errorDescription = error.response.data.message;
-        setErrorMessage("errorDescription");
+        setErrorMessage(errorDescription);
       });
 
     //APUNTES
