b048caf263251685d19bd9cb7e729542eb159838
Frank Gimeno <frankgimeno3@gmail.com>
2023-03-09 15:41:51 +0100

    que no se sub

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,15 +15,17 @@ function Navbar() {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="collapse navbar-collapse flexrow" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-          <Link to="/"><a className="nav-link active" aria-current="page" href="/">Dashboard</a> </Link>
-          <Link to="/profile"><a className="nav-link" href="/profile">Profile</a></Link>
-          <Link to="/signup"><a className="nav-link" href="/signup">Signup</a></Link>
-          <Link to="/login"><a className="nav-link" href="/login">LogIn</a></Link>
-          <Link to="/coversations"><a className="nav-link" href="/signup">Conversations</a></Link>
-          <Link to="/"><a className="nav-link" href="/">LogOut</a></Link>
+            <Link to="/"><a className="nav-link active" aria-current="page" href="/">Dashboard</a> </Link>
+            <Link to="/profile"><a className="nav-link" href="/profile">Profile</a></Link>
+            <Link to="/coversations"><a className="nav-link" href="/signup">Conversations</a></Link>
           </div>
+            <div className="rightsided">
+              <Link to="/signup"><a className="nav-link" href="/signup">Signup</a></Link>
+              <Link to="/login"><a className="nav-link" href="/login">LogIn</a></Link>
+              <Link to="/"><a className="nav-link" href="/">LogOut</a></Link>
+            </div>
         </div>
       </div>
     </nav>
