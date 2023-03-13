14a0de91cb531416a70219f67d93c448339ccf06
Frank Gimeno <frankgimeno3@gmail.com>
2023-03-13 13:54:51 +0100

    cambios 13.3.23

--- src/components/CreateOfferForm/CreateOfferForm.jsx
+++ src/components/CreateOfferForm/CreateOfferForm.jsx
@@ -1,56 +1,134 @@
-import React from 'react';
+import React, { useState } from "react";
+import ShipmentsService from "../../services/shipments.service";
 import './CreateOfferForm.css';
 
 function CreateOfferForm() {
+  const [formValues, setFormValues] = useState({
+    pickUpDireccion: "",
+    pickUpProvince: "",
+    deliveryDireccion: "",
+    deliveryProvince: "",
+    pallets: 0,
+    creationDate: "2022-12-12"
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const shipment = {
+      pickUpDireccion: formValues.pickUpDireccion,
+      pickUpProvince: formValues.pickUpProvince,
+      deliveryDireccion: formValues.deliveryDireccion,
+      deliveryProvince: formValues.deliveryProvince,
+      pallets: formValues.pallets,
+      creationDate: formValues.creationDate
+    };
+  const shipmentsService = new ShipmentsService("your-token-here");
+  shipmentsService
+    .addShipments(shipment)
+    .then((response) => {
+      console.log(response.data);
+      setFormValues({
+        pickUpDireccion: "",
+        pickUpProvince: "",
+        deliveryDireccion: "",
+        deliveryProvince: "",
+        pallets: 0,
+        creationDate: "2022-12-12"
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
   return (
-    <form>
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlInput1" className="form-label">Origin</label>
-      <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Insert origin city"/>
-    </div>
-    
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlInput1" className="form-label">Departure Date</label>
-      <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="DD/MM/YYYY"/>
-    </div>
-
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlInput1" className="form-label">Departure Hour</label>
-      <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="Approx departure hour"/>
-    </div>
-
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlInput1" className="form-label">Destination</label>
-      <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Insert destination city"/>
-    </div>
-
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlInput1" className="form-label">ArriVal Date</label>
-      <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="DD/MM/YYYY"/>
-    </div>
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlInput1" className="form-label">ArriVal Hour</label>
-      <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="Approx departure hour"/>
-    </div>
-
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlTextarea1" className="form-label">Available Space</label>
-      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlTextarea1" className="form-label">Coments</label>
-      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-
-    <button className="learn-more" type="submit">
-      <span className="circle" aria-hidden="true">  
-        <span className="icon arrow"></span>
-      </span>  
-      
-      <span className="button-text">Create Offer</span>
-    </button>
-
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="pickUpDireccionInput" className="form-label">Pick up direction</label>
+        <input
+          type="text"
+          className="form-control"
+          id="pickUpDireccionInput"
+          name="pickUpDireccion"
+          value={formValues.pickUpDireccion}
+          onChange={handleInputChange}
+          placeholder="Insert origin city"
+        />
+      </div>
+  
+      <div className="mb-3">
+        <label htmlFor="pickUpProvinceInput" className="form-label">Pick up province</label>
+        <input
+          type="text"
+          className="form-control"
+          id="pickUpProvinceInput"
+          name="pickUpProvince"
+          value={formValues.pickUpProvince}
+          onChange={handleInputChange}
+          placeholder="Insert origin province"
+        />
+      </div>
+  
+      <div className="mb-3">
+        <label htmlFor="deliveryDireccionInput" className="form-label">Delivery direction</label>
+        <input
+          type="text"
+          className="form-control"
+          id="deliveryDireccionInput"
+          name="deliveryDireccion"
+          value={formValues.deliveryDireccion}
+          onChange={handleInputChange}
+          placeholder="Insert destination city"
+        />
+      </div>
+  
+      <div className="mb-3">
+        <label htmlFor="deliveryProvinceInput" className="form-label">Delivery province</label>
+        <input
+          type="text"
+          className="form-control"
+          id="deliveryProvinceInput"
+          name="deliveryProvince"
+          value={formValues.deliveryProvince}
+          onChange={handleInputChange}
+          placeholder="Insert destination province"
+        />
+      </div>
+  
+      <div className="mb-3">
+        <label htmlFor="palletsInput" className="form-label">Number of pallets</label>
+        <input
+          type="number"
+          className="form-control"
+          id="palletsInput"
+          name="pallets"
+          value={formValues.pallets}
+          onChange={handleInputChange}
+          placeholder="Insert number of pallets"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="creationDateInput" className="form-label">Creation Date</label>
+        <input
+          type="date"
+          className="form-control"
+          id="creationDateInput"
+          name="creationDate"
+          value={formValues.creationDate}
+          onChange={handleInputChange}
+          placeholder="Insert number of pallets"
+        />
+      </div>
+  
+      <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   );
 }
--- src/services/shipments.service.js
+++ src/services/shipments.service.js
@@ -6,17 +6,17 @@ class ShipmentsService {
         this.headerObject = {headers: {authorization: `Bearer ${token}`}}
     }
     getShipments() {
-        return axios.get(process.env.REACT_APP_API_URL+"/shipments", this.headerObject);
+        return axios.get(process.env.REACT_APP_SERVER_URL+"/api/shipment", this.headerObject);
     }
     addShipments(shipment) {
-        return axios.post(process.env.REACT_APP_API_URL+"/shipments/new", shipment, this.headerObject);
+        return axios.post(process.env.REACT_APP_SERVER_URL+"/api/shipment/new", shipment, this.headerObject);
     }
     editShipments(shipmentId, shipment) {
-        return axios.put(process.env.REACT_APP_API_URL+`/shipments/edit/${shipmentId}`, shipment, this.headerObject);
+        return axios.put(process.env.REACT_APP_SERVER_URL+`/api/shipment/edit/${shipmentId}`, shipment, this.headerObject);
 
     }
     deleteShipments(shipmentId) {
-        return axios.delete(process.env.REACT_APP_API_URL+`/shipments/delete/${shipmentId}`, this.headerObject);
+        return axios.delete(process.env.REACT_APP_SERVER_URL+`/api/shipment/delete/${shipmentId}`, this.headerObject);
     }
 }
 
