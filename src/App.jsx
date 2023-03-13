8e18dc5972bd20ecbc389f4bd7160c69cad440e9
Eduardo Pizarro <eduardo-pizarro@hotmail.es>
2023-03-13 11:57:31 +0100

    con app.jsx arreglada

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-
 import Dashboard from "./pages/Dashboard/Dashboard";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-
+import SignupTranspPage from "./pages/SignupTranspPage/SignupPageTransp";
+import LoginTranspPage from "./pages/LoginTranspPage /LoginTranspPage";
 import Navbar from "./components/Navbar/Navbar";
 import IsPrivate from "./components/IsPrivate/IsPrivate";
 import IsAnon from "./components/IsAnon/IsAnon";
 import LandingPage from "./pages/LandingPage/LandingPage";
-
 function App() {
   return (
     <div className="App">
       <Navbar />
-
       <Routes>
         {/* <Route path="/" element={<HomePage />} /> */}
         <Route path="/" element={<IsAnon><LandingPage /></IsAnon>} />
@@ -23,12 +21,10 @@ function App() {
         <Route path="/profile" element={<IsPrivate><ProfilePage /></IsPrivate>} />
         <Route path="/signup" element={<IsAnon><SignupPage /> </IsAnon>} />
         <Route path="/login" element={<IsAnon> <LoginPage /> </IsAnon>} />
+        <Route path="transportist/signup" element={<IsAnon><SignupTranspPage /> </IsAnon>} />
+        <Route path="transportist/login" element={<IsAnon> <LoginTranspPage /> </IsAnon>} />
       </Routes>
     </div>
   );
 }
-
 export default App;
-
-
-
