9f1607d56386b83d90dc64464f188da445b3d952
Ivan <Ivandgea@gmail.com>
2023-03-14 09:56:11 +0100

    arreglado

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -45,47 +45,47 @@ function LoginPage() {
   };
 
   return (
-    <>
-      <div className="LoginPage">
-        <h1>Login as a Sender</h1>
+
+    <div className="LoginPage">
+      <h1>Login as a Sender</h1>
 
 
-        <div className="Loading Page">
-          <form className="px-4 py-3" onSubmit={handleLoginSubmit}>
-            <div className="mb-3">
-              <label htmlFor="exampleDropdownFormEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" id="exampleDropdownFormEmail1" name="email" value={email} onChange={handleEmail} placeholder="email@example.com" />
+      <div className="Loading Page">
+        <form className="px-4 py-3" onSubmit={handleLoginSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleDropdownFormEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleDropdownFormEmail1" name="email" value={email} onChange={handleEmail} placeholder="email@example.com" />
+          </div>
+          <div className="mb-3">
+            <label htmlFOR="exampleDropdownFormPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleDropdownFormPassword1" name="password" value={password} onChange={handlePassword} placeholder="Password" />
+          </div>
+          <div className="mb-3">
+            <div className="form-check">
+              <input type="checkbox" className="form-check-input" id="dropdownCheck" />
+              <label className="form-check-label" HTMLfor="dropdownCheck">
+                Remember me
+              </label>
             </div>
-            <div className="mb-3">
-              <label htmlFOR="exampleDropdownFormPassword1" className="form-label">Password</label>
-              <input type="password" className="form-control" id="exampleDropdownFormPassword1" name="password" value={password} onChange={handlePassword} placeholder="Password" />
-            </div>
-            <div className="mb-3">
-              <div className="form-check">
-                <input type="checkbox" className="form-check-input" id="dropdownCheck" />
-                <label className="form-check-label" HTMLfor="dropdownCheck">
-                  Remember me
-                </label>
-              </div>
-            </div>
-            <button type="submit" className="btn btn-primary">Log In</button>
-          </form>
-          {errorMessage && <p className="error-message">{errorMessage}</p>}
-          <div className="dropdown-divider"></div>
-          <p>New around here?</p>
-          <Link to={"/signup"} className="dropdown-item">  Sign up  </Link>
-        </div>
-
+          </div>
+          <button type="submit" className="btn btn-primary">Log In</button>
+        </form>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <div className="dropdown-divider"></div>
+        <p>New around here?</p>
+        <Link to={"/signup"} className="dropdown-item">  Sign up  </Link>
         <img src="https://www.rlicorp.com/sites/default/files/banner-images/LossControl_WebsiteHeader_02_72dpi_1.jpg" alt="banner" className="landingjpg" />
+      </div>
+    </div>
 
 
 
 
-      </>
-      );
+
+  );
 }
 
-      export default LoginPage;
+export default LoginPage;
 
 
 
